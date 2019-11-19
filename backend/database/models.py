0a7cdbd6fd0c8ddba1340f755975820a79506c9b
thinkpad <peeter._@hotmail.com>
2019-11-20 04:16:26 +1100

    Hacky way of recording response

--- backend/database/models.py
+++ backend/database/models.py
@@ -7,3 +7,14 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Status_tracker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    _200 = db.Column(db.Integer, default=0)
+    _201 = db.Column(db.Integer, default=0)
+    _400 = db.Column(db.Integer, default=0)
+    _401 = db.Column(db.Integer, default=0)
+    _403 = db.Column(db.Integer, default=0)
+
+    def increment_status_code(self,code):
+        exec('self._%d += 1'%code)
--- backend/run.py
+++ backend/run.py
@@ -11,19 +11,37 @@ TOKEN_DURATION = 60 * 2
 app = Flask(__name__)
 app.config.from_object(SQLAchemy_Config)
 
+# Setup database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+#initialize api directory
 from api import api
 api.init_app(app)
 
-from database import models
 
+# make life easier with flask shell
+from database import models
 @app.shell_context_processor
 def make_shell_context():
     return {
         'models':models,
     }
+
+from database.models import Status_tracker
+# create tracker if doesn't exist
+if not Status_tracker.query.all():
+    status_tracker = Status_tracker()
+    db.session.add(status_tracker)
+    db.session.commit()
+
+# record status code
+@app.after_request
+def increment_status_code(response):
+    tracker = Status_tracker.query.all()[0]
+    tracker.increment_status_code(response.status_code)
+    db.session.commit()
+    return response
 
 if __name__=='__main__':
     app.run(debug=True)
