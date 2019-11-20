cb7f9456b506a58ce4e02c61f2fe1e3ee595d39d
kanishk1 <kanishk.purohit@hotmail.com>
2019-11-20 23:06:38 +1100

    Might have fixed the finalpy.py file

--- finalpy.py
+++ finalpy.py
@@ -8,43 +8,62 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils import shuffle
 from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 
-def load_final(final_path,split_percentage):
+def load_final(final_path, split_percentage):
     df = pd.read_csv(final_path)
 
     df = shuffle(df)
-    final_x = df.drop('revenue',axis=1).values
-    final_x = df.drop('cast',axis=1).values
-    final_x = df.drop('director',axis=1).values
-    final_y = df['revenue'].values
+    df = df.dropna(axis=0)
 
-    split_point = int(len(final_x) * split_percentage)
-    final_X_train = final_x[:split_point]
-    final_y_train = final_y[:split_point]
-    final_X_test = final_x[split_point:]
-    final_y_test = final_y[split_point:]
+    X = df.drop(['revenue','poster_path'], axis=1).apply(LabelEncoder().fit_transform).values
+    Y = df['revenue'].values
+    # X = df.drop('director',axis=1).values
 
-    return final_X_train, final_y_train, final_X_test, final_y_test
+    # columnsToEncode = ['cast', 'director', 'genres', 'original_language', 'poster_path',
+    #    'production_companies', 'release_date', 'status', 'title']
+    # # print(df['cast'].to_string())
+    #
+    # for feature in columnsToEncode:
+    #     le = LabelEncoder()
+    #     try:
+    #         df[feature] = le.fit_transform(df[feature])
+    #     except:
+    #         print('Error encoding ' + feature)
+    # df.head()
+
+    # split_point = int(len(X) * split_percentage)
+    # final_X_train = X[:split_point]
+    # final_y_train = Y[:split_point]
+    # final_X_test = X[split_point:]
+    # final_y_test = Y[split_point:]
+    #
+    # return final_X_train, final_y_train, final_X_test, final_y_test
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+    return X_train, X_test, Y_train, Y_test
 
 
 if __name__ == '__main__':
 
-    csv_file = 'final.csv'
-    final_X, final_y, _, _ = load_final(csv_file,split_percentage=0.7)
+    csv_file = 'final_v2.csv'
+    final_X, _, final_y, _ = load_final(csv_file, split_percentage=0.7)
 
     classifiers = [KNeighborsClassifier(),
                    DecisionTreeClassifier(),
                    LinearDiscriminantAnalysis(),
-                   LogisticRegression(),
                    GaussianNB(),
-                   SVC()]
+                   SVC()
+                   ]
 
     classifier_accuracy_list = []
     for i, classifier in enumerate(classifiers):
-        accuracies = cross_val_score(classifier, final_X, final_y, cv=5)        #check this line for errors
+        print(classifier)
+        accuracies = cross_val_score(classifier, final_X, final_y, cv=5)  # check this line for errors
         classifier_accuracy_list.append((accuracies.mean(), type(classifier).__name__))
-    #
-    # classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
-    # for item in classifier_accuracy_list:
-        # print(item[1], ':', item[0])
+
+    classifier_accuracy_list = sorted(classifier_accuracy_list, reverse=True)
+    for item in classifier_accuracy_list:
+        print(item[1], ':', item[0])
